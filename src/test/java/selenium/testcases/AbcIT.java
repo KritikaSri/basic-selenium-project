0768d004ad25015a277ba550a4e2c257088fd6d6
Subir Chandra Ghosh <subir0071@users.noreply.github.com>
2019-01-29 15:55:29 +0530

    Update AbcIT.java

--- src/test/java/selenium/testcases/AbcIT.java
+++ src/test/java/selenium/testcases/AbcIT.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.junit.Assert;
 
 public class AbcIT {
 
